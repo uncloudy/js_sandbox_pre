796ccf512463559eb8172623be562bf06fd8ea9d
uncloudy <71613233+uncloudy@users.noreply.github.com>
2020-12-29 22:44:12 +0900

    js lecture

---
+++ 02_variable/index.js
@@ -0,0 +1,14 @@
+// variable : something can be changed
+
+// a = 87;
+// b = a - 7;
+// console.log(b);
+
+// 1. creat variable
+// 2. initialize
+// 3. use
+
+let a = 87;
+let b = a - 7;
+a = 4;
+console.log(b, a); //80 4
---
+++ 03_var_let_const/index.js
@@ -0,0 +1,7 @@
+// const : constant; unchangable
+//  let : changable
+
+const a = 87;
+let b = a - 7;
+a = 4;
+console.log(b, a);
